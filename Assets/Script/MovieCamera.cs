f8ba64943e798fb303795e86f1f198e6324584b7
yu13473054 <849460181@qq.com>
2017-06-07 22:42:34 +0800

    镜头从远大近运动

---
+++ Assets/Script/MovieCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovieCamera : MonoBehaviour {
+	public float speed = 10;
+
+	private float endZ=-20;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("test="+Time.deltaTime);
+		if(transform.position.z<-20){
+			transform.Translate (Vector3.forward*speed*Time.deltaTime);
+		}
+	}
+}

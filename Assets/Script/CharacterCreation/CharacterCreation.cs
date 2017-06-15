83de54d6410a578eac9bed867bf8e34a4c28811c
yu13473054 <849460181@qq.com>
2017-06-15 22:46:36 +0800

    增加角色创建场景，实现角色选择逻辑

---
+++ Assets/Script/CharacterCreation/CharacterCreation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCreation : MonoBehaviour {
+	public GameObject[] characters;
+	public int characterId;
+
+	private GameObject[] chaObjs;
+	private int length;
+
+	// Use this for initialization
+	void Start () {
+		length = characters.Length;
+		chaObjs = new GameObject[length];
+		for(int i=0; i<length;i++){
+			//实例化对象
+			chaObjs[i] = GameObject.Instantiate (characters [i], transform.position, transform.rotation) as GameObject;
+		}
+		UpdateCha ();
+	}
+
+	//显示角色
+	void UpdateCha(){
+		for(int i= 0; i<length; i++){
+			if (i == characterId) {
+				chaObjs [i].SetActive (true);
+			} else {
+				chaObjs [i].SetActive (false);
+			}
+		}
+	}
+
+	public void next(){
+		characterId++;
+//		characterId %= length;
+		if(characterId>length-1){
+			characterId = 0;
+		}
+		UpdateCha ();
+	}
+
+	public void prev(){
+		characterId--;
+		if(characterId<0){
+			characterId = length-1;
+		}
+		UpdateCha ();
+	}
+
+}
--- Assets/Script/Start/GameButtonLogic.cs
+++ Assets/Script/Start/GameButtonLogic.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class GameButtonLogic : MonoBehaviour {
 
 	public void NewGameBtnDeal(){//加载新游戏的按钮的处理
-		
+		Debug.Log("NewGameBtnDeal点击了！");
 	}
 
 	public void LoadGameBtnDeal(){//重载已经存储的场景的处理
-		
+		Debug.Log("LoadGameBtnDeal点击了！");
 	}
 
 }
